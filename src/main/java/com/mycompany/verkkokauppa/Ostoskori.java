ed4ad088e3ff4c4952d5da8bdf888f122fd72a47
Pebe <Nabscina@users.noreply.github.com>
2016-12-10 19:38:23 +0100

    ran tests

--- src/main/java/com/mycompany/verkkokauppa/Ostoskori.java
+++ src/main/java/com/mycompany/verkkokauppa/Ostoskori.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.verkkokauppa;
 
-/**
- *
- * @author Peppi
- */
+import java.util.List;
+
 public class Ostoskori {
-    
+
+    public int tuotteitaKorissa() {
+        // kertoo korissa olevien tuotteiden määrän
+        // metodin nimi on hieman huono, kyseessä oikeastaan koriin lisättyjen "asioiden" määrä
+        // eli jos koriin lisätty 2 kpl tuotetta "Koff", tulee metodin palauttaa 2     
+
+        return -1;
+    }
+
+    public int hinta() {
+        // kertoo korissa olevien tuotteiden yhteenlasketun hinnan
+
+        return -1;
+    }
+
+    public void lisaaTuote(Tuote lisattava) {
+        // lisää tuotteen
+    }
+
+    public void poista(Tuote poistettava) {
+        // poistaa tuotteen
+    }
+
+    public List<Ostos> ostokset() {
+        // palauttaa listan jossa on korissa olevat ostokset
+
+        return null;
+    }
+
+    public void tyhjenna() {
+        // tyhjentää korin
+    }
 }
